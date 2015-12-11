56a14253306b59e0be74e686bdd2fe12ab364be8
Rina Ahmed <rina.ahmed@live.com>
2015-12-11 21:48:54 +0100

    Added ng routing and partial templates

--- javascripts/chirpApp.js
+++ javascripts/chirpApp.js
@@ -1,5 +1,24 @@
 //chirpApp.js
-var app = angular.module('chirpApp', []);
+var app = angular.module('chirpApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	//the timeline display
+	.when('/', {
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: 'login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: 'register.html',
+      controller: 'authController'
+    });	
+});
 
 app.controller('mainController', function($scope){
 	$scope.posts = [];
@@ -10,5 +29,20 @@ app.controller('mainController', function($scope){
 		$scope.posts.push($scope.newPost);
 		$scope.newPost = {created_by:'', text:'', created_at:''};
 	};
-	
 });
+
+app.controller('authController', function($scope){
+	$scope.user = {username:'', password:''};
+	$scope.error_message = '';
+	
+	$scope.login = function(){
+		//placeholder until auth is implemented
+		$scope.error_message = 'login request for '+ $scope.user.username;	
+	};
+	
+	$scope.register = function(){
+		//placeholder
+		$scope.error_message = 'register request for '+ $scope.user.username;
+	};
+
+});	
